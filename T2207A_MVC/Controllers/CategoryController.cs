f76050889fb833ac9480a21f724ff8f5a65361ae
ngomanhson <ngomanhson2004txpt@gmail.com>
2023-09-08 10:05:28 +0700

    CRUD Category

--- T2207A_MVC/Controllers/CategoryController.cs
+++ T2207A_MVC/Controllers/CategoryController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using T2207A_MVC.Entities;
+using T2207A_MVC.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -32,10 +33,56 @@ namespace T2207A_MVC.Controllers
             return View();
         }
 
-        public IActionResult Edit()
+
+        [HttpPost]
+        public IActionResult Create(CategoryViewModel model)
         {
-            return View();
+            if (ModelState.IsValid) // Viladate
+            {
+                _context.Categories.Add(new Category { name = model.name });
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
         }
+
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            Category category = await _context.Categories.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(new CategoryEditModel { id = category.id, name = category.name});
+        }
+
+        [HttpPost]
+        public IActionResult Edit(CategoryEditModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Update(new Category { id = model.id, name = model.name });
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            Category category = _context.Categories.Find(id);
+            if(category == null)
+            {
+                return NotFound();
+            }
+            _context.Categories.Remove(category);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // Soft delete
+
     }
 }
 
---
+++ T2207A_MVC/Models/CategoryEditModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace T2207A_MVC.Models
+{
+	public class CategoryEditModel
+	{
+		[Required]
+		public int id { get; set; }
+
+        [Required(ErrorMessage = "Please enter a category name")]
+        [MinLength(6, ErrorMessage = "Please enter a minimum of 6 characters")]
+        [Display(Name = "Name")]
+        public string name { get; set; }
+    }
+}
+
