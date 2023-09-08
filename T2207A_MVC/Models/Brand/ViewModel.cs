38ee41c9ca52cb26daa0779d5a1343f0c901364b
ngomanhson <ngomanhson2004txpt@gmail.com>
2023-09-08 12:48:54 +0700

    CURD Brand #2

--- T2207A_MVC/Models/Brand/ViewModel.cs
+++ T2207A_MVC/Models/Brand/ViewModel.cs
@@ -4,7 +4,7 @@ namespace T2207A_MVC.Models.Brand
 {
 	public class ViewModel
 	{
-        [Required(ErrorMessage = "Please enter a category name")]
+        [Required(ErrorMessage = "Please enter a brand name")]
         [MinLength(6, ErrorMessage = "Please enter a minimum of 6 characters")]
         [Display(Name = "Name")]
         public string name { get; set; }
