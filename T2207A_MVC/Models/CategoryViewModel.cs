c4e55660ab9efd8a6c2ad765538997bfc9afb113
ngomanhson <ngomanhson2004txpt@gmail.com>
2023-09-08 09:04:51 +0700

    Create form

---
+++ T2207A_MVC/Models/CategoryViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace T2207A_MVC.Models
+{
+	public class CategoryViewModel
+	{
+		[Required(ErrorMessage = "Please enter a category name")]
+		[MinLength(6, ErrorMessage = "Please enter a minimum of 6 characters")]
+		[Display(Name = "Name")]
+		public string name { get; set; }
+	}
+}
+
