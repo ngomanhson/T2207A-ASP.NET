14dfc4f5766ddf41131b2ff164063ae76955c772
ngomanhson <ngomanhson2004txpt@gmail.com>
2023-09-18 21:26:53 +0700

    API

---
+++ T2207A_API/Controllers/ProductController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using T2207A_API.Entities;
+using T2207A_API.DTOs;
+using T2207A_API.Models.Product;
+using T2207A_API.Models.Category;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace T2207A_API.Controllers
+{
+    [ApiController]
+    [Route("/api/product")]
+    public class ProductController : Controller
+    {
+        private readonly T2207aApiContext _context;
+     
+        public ProductController(T2207aApiContext context) // reflection
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<Product> products = _context.Products.ToList();
+
+            List<ProductDTO> data = new List<ProductDTO>();
+            foreach (Product p in products)
+            {
+                data.Add(new ProductDTO { id = p.Id, name = p.Name, price = p.Price, qty = p.Qty, description = p.Description, thumbnai = p.Thumbnail, category = p.CategoryId });
+            }
+            return Ok(data);
+        }
+
+        [HttpGet]
+        [Route("get-by-id")]
+        public IActionResult Get(int id)
+        {
+            try
+            {
+                Product p = _context.Products.Find(id);
+                if (p != null)
+                {
+                    return Ok(new ProductDTO { id = p.Id, name = p.Name, price = p.Price, qty = p.Qty, description = p.Description, thumbnai = p.Thumbnail, category = p.CategoryId });
+                }
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+            return NotFound();
+        }
+
+
+        [HttpPost]
+        public IActionResult Create(CreateProduct model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    Product data = new Product { Name = model.name , Price = model.price, Qty = model.qty, Description = model.description, Thumbnail = model.thumbnai, CategoryId = model.category};
+                    _context.Products.Add(data);
+                    _context.SaveChanges();
+                    return Created($"get-by-id?id={data.Id}", new ProductDTO { id = data.Id, name = data.Name, price = data.Price, qty = data.Qty, description = data.Description, thumbnai = data.Thumbnail, category = data.CategoryId });
+                }
+                catch (Exception e)
+                {
+                    return BadRequest(e.Message);
+                }
+            }
+            var msgs = ModelState.Values.SelectMany(v => v.Errors).Select(v => v.ErrorMessage);
+            return BadRequest(string.Join(" | ", msgs));
+        }
+    }
+}
+
---
+++ T2207A_API/DTOs/ProductDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace T2207A_API.DTOs
+{
+	public class ProductDTO
+	{
+        public int id { get; set; }
+        public string name { get; set; }
+        public decimal price { get; set; }
+        public string description { get; set; }
+        public string thumbnai { get; set; }
+        public int qty { get; set; }
+        public int category { get; set; }
+    }
+}
+
